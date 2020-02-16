4f1d9ed8efb32a43de4083424204eb795c21fb3d
Hugues <hugues@cocktail.cc>
2020-02-16 13:42:40 +0100

    course 17 - Exercice add publisher entity

--- src/main/java/guru/springframework/spring5webapp/boostrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/boostrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.boostrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -35,7 +39,12 @@ public class BootStrapData implements CommandLineRunner {
         authorRepository.save(rod);
         bookRepository.save(noEJB);
 
+        Publisher oreil = new Publisher("O'reilly", "23st Street 12", "New York", "NY", "123456");
+        publisherRepository.save(oreil);
+
+
         System.out.println("Started In BootStrap");
         System.out.println("Number of books :"+bookRepository.count());
+        System.out.println("Number of publisher :"+publisherRepository.count());
     }
 }
